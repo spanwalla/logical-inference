0d7fe835e0e2d8cb294134e06f4a4187ea03a887
spanwalla <lus550@yandex.ru>
2024-11-23 09:16:23 +0300

    исправил ApplyModusPonens

--- internal/inference_rules/inference_rules.go
+++ internal/inference_rules/inference_rules.go
@@ -7,35 +7,39 @@ import (
 
 func ApplyModusPonens(lhs expression.Expression, rhs expression.Expression) expression.Expression {
 	if lhs.Empty() || rhs.Empty() {
-		return expression.Expression{}
+		return expression.NewExpression()
 	}
 	if rhs.Nodes[0].Term.Op != expression.Implication {
-		return expression.Expression{}
+		return expression.NewExpression()
 	}
-	//попытка применить унификацию
+	// Попытка применить унификацию
 	substitution := make(map[expression.Value]expression.Expression)
 	if !helper.GetUnification(lhs, rhs.CopySubtree(rhs.Subtree(0).Left()), &substitution) {
-		return expression.Expression{}
+		return expression.NewExpression()
 	}
 	result := rhs
 	result.ChangeVariables(lhs.MaxValue() + 1)
 	vars := result.Variables()
 
-	for var_ := range vars {
-		if _, exist := substitution[expression.Value(var_)]; !exist {
+	for _, var_ := range vars {
+		if _, exist := substitution[var_]; !exist {
 			continue
 		}
-		change := substitution[expression.Value(var_)]
-		_, exists := substitution[change.Nodes[0].Term.Val]
-		for change.Nodes[0].Term.Type == expression.Variable && exists {
+		change := substitution[var_]
+		for change.Nodes[0].Term.Type == expression.Variable {
+			if _, exists := substitution[change.Nodes[0].Term.Val]; !exists {
+				break
+			}
+
 			shouldNegate := change.Nodes[0].Term.Op == expression.Negation
 			change = substitution[change.Nodes[0].Term.Val]
 			if shouldNegate {
 				change.Negation(0)
 			}
 		}
-		result.Replace(expression.Value(var_), &change)
+		result.Replace(var_, &change)
 	}
+
 	result = result.CopySubtree(result.Subtree(0).Right())
 	result.Normalize()
 	return result
