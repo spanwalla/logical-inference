1de343e7190bdb70f89afa443493e09123524aff
Sorkan176 <ansemenenko176@gmail.com>
2024-11-23 06:37:30 +0300

    Solver

---
+++ internal/rules/rules.go
@@ -0,0 +1,7 @@
+package rules
+
+import "logical-inference/internal/expression"
+
+func ApplyModusPonens(left, right expression.Expression) expression.Expression {
+	return expression.NewExpression()
+}
